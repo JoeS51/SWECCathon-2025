68f938df4b087785db56c889151ab609ee3ad6c2
Johannes Sluis <joesluis51@gmail.com>
2025-04-24 00:03:38 -0700

    changed dir structure to wxt

--- scripts/popup.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-document.getElementById("extract").addEventListener("click", () => {
-    console.log("HII")
-});
-=======
-console.log("POPPP UP, WITH GUSTO");
-
->>>>>>> 9460391827ff74b8bb60f7e9c034aa7ff96e52e2
